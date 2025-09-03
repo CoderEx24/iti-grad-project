6c684c5d94a213614b3056d2d39c65557ab76c26
CoderEx24 <coderex24@protonmail.com>
2025-09-04 02:11:14 +0300

    implemented simple ViewModel for AuthActivity
    I'm yet to figure out how to instantiate the `PreferenceManager`

--- app/src/main/java/com/example/iti_grad_project/ui/viewmodels/AuthViewModel.kt
+++ app/src/main/java/com/example/iti_grad_project/ui/viewmodels/AuthViewModel.kt
@@ -1,6 +1,51 @@
 package com.example.iti_grad_project.ui.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.iti_grad_project.repositories.AuthRepository
 
-class AuthViewModel: ViewModel() {
+data class AuthActivityUiState(
+    val username: String = "",
+    val password: String = "",
+)
+
+class AuthViewModel(
+    private val repository: AuthRepository
+): ViewModel() {
+    val uiState: MutableLiveData<AuthActivityUiState> = MutableLiveData(AuthActivityUiState())
+        private set
+
+    var username: String
+        set(value) {
+            val state = uiState.value
+            uiState.postValue(state!!.copy(username = value))
+        }
+        get() = uiState.value!!.username
+
+    var password: String
+        set(value) {
+            val state = uiState.value
+            uiState.postValue(state!!.copy(password = value))
+        }
+        get() = uiState.value!!.password
+
+    companion object {
+        val Factory = object : ViewModelProvider.Factory {
+
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+                val viewModel = AuthViewModel(
+                    AuthRepository(
+                        TODO(),
+                        TODO()
+                    )
+                )
+
+                return viewModel as T
+            }
+        }
+    }
 }
