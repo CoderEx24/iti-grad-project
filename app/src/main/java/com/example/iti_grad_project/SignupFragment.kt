0e02730aced8f5488594b5ee1ee45f40ddbf8b35
CoderEx24 <coderex24@protonmail.com>
2025-09-03 07:27:00 +0300

    added scaffold for SignupFragment

---
+++ app/src/main/java/com/example/iti_grad_project/SignupFragment.kt
@@ -0,0 +1,10 @@
+package com.example.iti_grad_project
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SignupFragment : Fragment(R.layout.fragment_signup) {
+}
