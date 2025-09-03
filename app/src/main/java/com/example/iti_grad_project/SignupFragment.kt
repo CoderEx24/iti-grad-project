995dbe223e1f410f5d2671b987e3b602c3b1f4f8
CoderEx24 <coderex24@protonmail.com>
2025-09-03 23:07:40 +0300

    SignupFragment: implemented checking whether the two passwords match

--- app/src/main/java/com/example/iti_grad_project/SignupFragment.kt
+++ app/src/main/java/com/example/iti_grad_project/SignupFragment.kt
@@ -1,10 +1,53 @@
 package com.example.iti_grad_project
 
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import android.text.TextWatcher
 
 class SignupFragment : Fragment(R.layout.fragment_signup) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val passwordTextView: TextView = view.findViewById(R.id.fragment_signup_password)
+        val confirmPasswordTextView: TextView = view.findViewById(R.id.fragment_signup_confirmPassword)
+
+        val passwordsMatchWatcher = object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                val password = passwordTextView.text
+                val confirmPassword = confirmPasswordTextView.text
+
+                if (!password.isEmpty() && !confirmPassword.isEmpty() &&
+                    !password.contentEquals(confirmPassword)) {
+                    confirmPasswordTextView.error = "Passwords don't match"
+                } else {
+                    confirmPasswordTextView.error = null
+                }
+            }
+
+            override fun beforeTextChanged(
+                p0: CharSequence?,
+                p1: Int,
+                p2: Int,
+                p3: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                p0: CharSequence?,
+                p1: Int,
+                p2: Int,
+                p3: Int
+            ) {
+            }
+        }
+
+        passwordTextView.addTextChangedListener(passwordsMatchWatcher)
+        confirmPasswordTextView.addTextChangedListener(passwordsMatchWatcher)
+    }
+
 }
