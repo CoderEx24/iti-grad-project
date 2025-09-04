932904640074c5dee8dc60db00531c6860bf3cc8
CoderEx24 <coderex24@protonmail.com>
2025-09-04 23:00:56 +0300

    implemented instantiating AuthViewModel

--- app/src/main/java/com/example/iti_grad_project/ui/activities/AuthActivity.kt
+++ app/src/main/java/com/example/iti_grad_project/ui/activities/AuthActivity.kt
@@ -7,10 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import android.content.Context
+import androidx.lifecycle.viewmodel.MutableCreationExtras
 import com.example.iti_grad_project.R
 import androidx.navigation.findNavController
+import com.example.iti_grad_project.ui.viewmodels.AuthViewModel
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var authViewModel: AuthViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,6 +28,14 @@ class AuthActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        authViewModel = ViewModelProvider.create(
+            this as ViewModelStoreOwner,
+            factory = AuthViewModel.Factory,
+            extras = MutableCreationExtras().apply {
+                set(AuthViewModel.CONTEXT_KEY, this@AuthActivity.applicationContext)
+            }
+        )[AuthViewModel::class]
     }
 
     fun onGoToSignup(view: View?) {
