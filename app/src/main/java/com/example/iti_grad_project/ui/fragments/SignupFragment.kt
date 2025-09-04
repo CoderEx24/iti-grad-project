6e092f185d63408bbd8111463c077ddbcbffb0a3
CoderEx24 <coderex24@protonmail.com>
2025-09-04 23:13:13 +0300

    implemented propageting the username and password from the SignupFragment to the AuthViewModel

--- app/src/main/java/com/example/iti_grad_project/ui/fragments/SignupFragment.kt
+++ app/src/main/java/com/example/iti_grad_project/ui/fragments/SignupFragment.kt
@@ -6,14 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.TextView
 import android.text.TextWatcher
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.activityViewModels
 import com.example.iti_grad_project.R
+import com.example.iti_grad_project.ui.viewmodels.AuthViewModel
 
 class SignupFragment : Fragment(R.layout.fragment_signup) {
 
+    private val authViewModel: AuthViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val usernameTextView: TextView = view.findViewById(R.id.fragment_signup_username)
         val passwordTextView: TextView = view.findViewById(R.id.fragment_signup_password)
         val confirmPasswordTextView: TextView = view.findViewById(R.id.fragment_signup_confirmPassword)
+
+        usernameTextView.addTextChangedListener {
+            authViewModel.username = it?.toString() ?: ""
+        }
+
+        passwordTextView.addTextChangedListener {
+            authViewModel.password = it?.toString() ?: ""
+        }
 
         val passwordsMatchWatcher = object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
