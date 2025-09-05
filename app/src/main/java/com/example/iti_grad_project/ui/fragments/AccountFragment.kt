4119778634266937083b22a433fbb49c231aef72
Zoon <zoonattack18@gmail.com>
2025-09-05 04:28:05 +0300

    -Created fragment_account.xml and it's design -Added navigation from account fragment to about app fragment

--- app/src/main/java/com/example/iti_grad_project/ui/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/iti_grad_project/ui/fragments/AccountFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.navigation.fragment.findNavController
 import com.example.iti_grad_project.R
 
 class AccountFragment : Fragment() {
@@ -14,5 +18,20 @@ class AccountFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var about = view.findViewById<LinearLayout>(R.id.btnAboutApp)
+        var signOut = view.findViewById<LinearLayout>(R.id.btnSignOut)
+
+        about.setOnClickListener {
+            Toast.makeText(requireContext(), R.string.about_app, Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.aboutFragment)
+        }
+        signOut.setOnClickListener {
+            Toast.makeText(requireContext(), "Signing out...", Toast.LENGTH_SHORT).show()
+            //Signout api call
+        }
     }
 }
