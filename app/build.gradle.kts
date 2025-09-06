c3f85d97179ba688d974d17cc4a4f8153198d7dd
Zoon <zoonattack18@gmail.com>
2025-09-06 20:01:03 +0300

    -Added splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,5 @@ dependencies {
     implementation(libs.material)
     implementation(libs.recyclerview)
     implementation(libs.glide)
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
